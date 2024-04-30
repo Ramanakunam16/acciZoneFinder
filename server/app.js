f2b82a63ab42c57c302ef8801918c3ef1476356e
ramanakunam16 <ramanakunam16@gmail.com>
2024-04-30 10:04:54 +0530

    cors updated

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "",
+    origin: ["https://acci-zone-finder.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
